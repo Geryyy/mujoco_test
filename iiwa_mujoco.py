3d52da5cfd994a25e93ec14c0f610f367d377c0a
Gerald Ebmer <gerald.ebmer@outlook.com>
2023-04-05 08:25:06 +0200

    added c++ sample; performance test: python 1670ms, c++ 1997ms

--- iiwa_mujoco.py
+++ iiwa_mujoco.py
@@ -1,9 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import arcpy
+# import arcpy
 import mujoco
 import mediapy as media
+import time
 
+
+### performance test
+# simulation duration 1000s
+# execution time: 1694ms
 
 # filename = "/home/geraldebmer/repos/test_mujoco/iiwa/robot.xml"
 filename = "robot.xml"
@@ -21,7 +26,7 @@ mujoco.mj_forward(model, data)
 renderer.update_scene(data)
 media.show_image(renderer.render())
 
-DURATION  = 3   # seconds
+DURATION  = 1000   # seconds
 FRAMERATE = 60  # Hz
 
 
@@ -31,18 +36,23 @@ frames = []
 i = 0
 
 # print(data.timestep)
+# get time in microseconds
+start = time.time()
 
 while data.time < DURATION:
   # Step the simulation.
   mujoco.mj_step(model, data)
-  print(str(i) +": "+ str(data.time), end='\r')
-  i = i +1
-  break
+  # print(str(i) +": "+ str(data.time), end='\r')
+  # i = i +1
+  # break
   # Render and save frames.
-  if len(frames) < data.time * FRAMERATE:
-    renderer.update_scene(data)
-    pixels = renderer.render()
-    frames.append(pixels)
+  # if len(frames) < data.time * FRAMERATE:
+  #   renderer.update_scene(data)
+  #   pixels = renderer.render()
+  #   frames.append(pixels)
+
+end = time.time()
+print("time: " + str(end - start))
 
 # Display video.
 # media.show_video(frames, fps=FRAMERATE)
